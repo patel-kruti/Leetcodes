3f8b8567c74423c854b157afa6219e99fbbcd042
KRUTI PATEL <patelkrutipravin@gmail.com>
2025-01-08 07:46:44 +0530

    Array, Backtracking - Medium Leetcode

---
+++ Medium_Level_using_Cpp/Recursion and Backtracking/39. Combination Sum.cpp
@@ -0,0 +1,27 @@
+// Problem : 39. Combination Sum
+// Link : 
+// Topics : Array, recursion, Backtracking
+// Time Complexity : O(2^T * k) -> k = average size of valid combination
+// Space Complexity : O(T) -> T = target/minCandidate
+// Solution :
+class Solution {
+    void backtrack(vector<vector<int>>& result, vector<int> path, int target, vector<int>& candidates, int index){
+        if(target == 0){
+            result.push_back(path);
+        } 
+        
+        for(int i = index; i < candidates.size(); i++){
+            if(candidates[i] > target) continue;
+            path.push_back(candidates[i]);
+            backtrack(result, path, target - candidates[i], candidates, i);
+            path.pop_back();
+        }
+        
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        backtrack(result, {}, target, candidates, 0);
+        return result;
+    }
+};
