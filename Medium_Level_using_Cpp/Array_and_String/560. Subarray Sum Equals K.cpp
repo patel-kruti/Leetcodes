a9e714671b8f5d12c993b428250cc254d8939053
KRUTI PATEL <patelkrutipravin@gmail.com>
2025-02-02 22:16:48 +0530

    Array and String - Medium Leetcode

---
+++ Medium_Level_using_Cpp/Array_and_String/560. Subarray Sum Equals K.cpp
@@ -0,0 +1,24 @@
+// Problem : 560. Subarray Sum Equals K
+// Link : https://leetcode.com/problems/subarray-sum-equals-k/
+// Topic : Array and String
+// Difficulty : Medium
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Solution : Using Hash Map
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> prefixSumFreq;
+        prefixSumFreq[0] = 1;
+        int result = 0, currSum = 0;
+
+        for(int num: nums){
+            currSum += num;
+            if(prefixSumFreq.find(currSum - k) != prefixSumFreq.end()){
+                result += prefixSumFreq[currSum - k];
+            }
+            prefixSumFreq[currSum]++;
+        }
+        return result;
+    }
+};
