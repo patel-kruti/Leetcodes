135a8ee167b8d2fdafec21bbe3494efd70a94a56
KRUTI PATEL <patelkrutipravin@gmail.com>
2025-02-03 14:47:32 +0530

    Array, In-Place, Matrix, Simulation - Medium Leetcode

---
+++ Medium_Level_using_Cpp/Array_and_String/289. Game of Life.cpp
@@ -0,0 +1,48 @@
+// Problem : 289. Game of Life
+// Link : https://leetcode.com/problems/game-of-life/
+// Topic : Array, In-place
+// Difficulty : Medium
+// Time Complexity : O(m*n)
+// Space Complexity : O(1)
+// Solution : In-place
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int rows = board.size();
+        int cols = board[0].size();
+
+        vector<int> directions = {-1, 0, 1};
+
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < cols; c++){
+
+                int liveNeigh = 0;
+
+                for(int dr: directions){
+                    for(int dc: directions){
+                        if(dr == 0 && dc == 0) continue;
+                        int nr = r + dr, nc = c + dc;
+                        if(nr >= 0 && nr < rows && nc >= 0 && nc < cols && abs(board[nr][nc]) == 1){
+                            liveNeigh++;
+                        }
+                    }
+                }
+
+                if(board[r][c] == 1 && (liveNeigh < 2 || liveNeigh > 3)){
+                    board[r][c] = -1;
+                }
+
+                if(board[r][c] == 0 && liveNeigh == 3){
+                    board[r][c] = 2;
+                }
+            }
+        }
+
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < cols; c++){
+                if(board[r][c] == -1) board[r][c] = 0;
+                if(board[r][c] == 2) board[r][c] = 1;
+            }
+        }
+    }
+};
